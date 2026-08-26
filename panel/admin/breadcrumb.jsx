66ee3ffa23b9e86e7d23216eb8bc337824313a96
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:00 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='creditScore'
+    name='creditManagement'
+    title='creditManagementCreditManagement'
+    {...props}
+/>
